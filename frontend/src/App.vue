99b4ee272fbb4f4716a4926533b1de25b1e3af5f
Addan Felipe Neri Andrade <addanfelipe@gmail.com>
2021-04-27 23:11:52 -0300

    feat(home/form): acoes com vuejs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,87 +1,18 @@
 <template>
-  <div class="text-center mt-3 mb-5">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <span style="font-size: 100px" class="text-secondary">345g</span>
-    <br />
-    <span style="font-size: 20px" class="text-secondary">Peso atual</span>
-  </div>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
-
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-4">
-        <form class="row">
-          <div class="mb-3">
-            <label class="form-label">Qtd. de gramas ideal</label>
-            <input class="form-control" type="number" />
-            <div class="form-text">O recipiente encherá até este peso</div>
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Qtd. de gramas minima</label>
-            <input class="form-control" type="number" />
-            <div class="form-text">
-              O recipiente encherá quando o peso estiver neste valor
-            </div>
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Verificar peso a cada (segundos)</label>
-            <input class="form-control" type="number" />
-            <div class="form-text">Ex.: 120s (2m)</div>
-          </div>
-
-          <div>
-            <label class="form-label">Momentos de verificação</label>
-            <div class="input-group mb-2">
-              <span class="input-group-text">Início e Fim</span>
-              <input type="time" aria-label="Início" class="form-control" />
-              <input type="time" aria-label="Fim" class="form-control" />
-            </div>
-            <div class="input-group mb-2">
-              <span class="input-group-text">Início e Fim</span>
-              <input type="time" aria-label="Início" class="form-control" />
-              <input type="time" aria-label="Fim" class="form-control" />
-            </div>
-            <div class="input-group mb-2">
-              <span class="input-group-text">Início e Fim</span>
-              <input type="time" aria-label="Início" class="form-control" />
-              <input type="time" aria-label="Fim" class="form-control" />
-            </div>
-            <div class="input-group mb-2">
-              <span class="input-group-text">Início e Fim</span>
-              <input type="time" aria-label="Início" class="form-control" />
-              <input type="time" aria-label="Fim" class="form-control" />
-            </div>
-          </div>
-          <div class="col-12">
-            <button type="button" class="col-12 btn btn-success">
-              Salvar alterações
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
+  <Home />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Home,
+  },
+  mounted() {
+    console.log("Automatic Feeder");
   },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="text-center mt-3 mb-5">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <span style="font-size: 100px" class="text-secondary">345g</span>
+    <br />
+    <span style="font-size: 20px" class="text-secondary">Peso atual</span>
+  </div>
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-4">
+        <form class="row">
+          <div class="mb-3">
+            <label class="form-label">Qtd. de gramas ideal</label>
+            <input class="form-control" type="number" />
+            <div class="form-text">Enquanto estiver sendo monitorado o recipiente estará com este peso</div>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label">Momentos de monitoramento</label>
+
+            <div class="input-group mb-2" v-for="(v, index) in verifications" :key="index">
+              <span class="input-group-text">Início e Fim</span>
+              <input type="time" aria-label="Início" class="form-control" v-model="v.start"/>
+              <input type="time" aria-label="Fim" class="form-control" v-model="v.end"/>
+              <button class="btn btn-danger" type="button" @click="actionVerificationDelete(index)">-</button>
+            </div>
+
+            <button class="btn btn-outline-success" type="button" @click="actionVerificationAdd()">+</button>
+          </div>
+
+          <hr/>
+
+          <div class="col-12">
+            <button type="button" class="col-12 btn btn-success" @click="actionSave()">
+              Salvar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+interface Verification {
+  start: string | null
+  end: string | null
+}
+
+const Home = defineComponent({
+  data() {
+    return {
+      verifications: [] as Verification[],
+    };
+  },
+  mounted() {
+    this.verifications = [
+      { start: "10:30", end: "12:30" },
+      { start: "15:30", end: "16:30" },
+      { start: "18:30", end: "20:30" },
+    ]
+  },
+  methods: {
+    actionVerificationAdd() {
+      this.verifications.push({ start: null, end: null })
+    },
+    actionVerificationDelete(index: number) {
+      this.verifications.splice(index, 1)
+    },
+    actionSave () {
+      
+    },
+  }
+})
+export default Home
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
